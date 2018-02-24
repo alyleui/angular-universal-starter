0e958c71c3ba89b76ae006679d08b4c68b01de4b
alyleui <alyleui@gmail.com>
2018-02-23 22:59:10 -0500

    test

--- functions/src/getdata.ts
+++ functions/src/getdata.ts
@@ -1,9 +1,9 @@
 import { readFileSync } from 'fs';
 import * as functions from 'firebase-functions';
-import { join } from 'path';
+import { join,resolve } from 'path';
 
 export const readData = functions.https.onRequest((req, res) => {
   const data = readFileSync(`${process.cwd()}/data.json`, 'utf8');
-  console.log(data);
+  console.log(JSON.parse(data));
   res.json(data);
 });
